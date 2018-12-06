e767c09cc2586d75fee5966523a25acea891b4a7
Saren <saren@KSHRD-PP-05s-iMac.local>
2018-12-06 17:06:11 +0700

    first commit

---
+++ CoreData001/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  CoreData001
+//
+//  Created by Saren on 12/5/18.
+//  Copyright © 2018 Saren. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
